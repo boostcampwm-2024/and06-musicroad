7289bdaf978238972e82812528d758f1122b3928
miller198 <miller198@naver.com>
2025-05-12 22:18:19 +0900

    [fix] 검색 버그 해결
    특정 단어 검색 시 튕기던 버그

--- core/model/src/main/java/com/squirtles/core/model/Song.kt
+++ core/model/src/main/java/com/squirtles/core/model/Song.kt
@@ -1,6 +1,8 @@
 package com.squirtles.core.model
 
 import kotlinx.serialization.Serializable
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 /**
  * 애플뮤직에서 불러온 노래 정보를 비즈니스 로직에서 사용하기 위해 변환한 클래스
@@ -22,4 +24,16 @@ data class Song(
         else imageUrl.replace("{w}", width.toString())
             .replace("{h}", height.toString())
     }
+
+    fun encoded(): Song = this.copy(
+        songName = URLEncoder.encode(songName, StandardCharsets.UTF_8.toString()).replace("+", "%20"),
+        artistName = URLEncoder.encode(artistName, StandardCharsets.UTF_8.toString()).replace("+", "%20"),
+        albumName = URLEncoder.encode(albumName, StandardCharsets.UTF_8.toString()).replace("+", "%20"),
+        imageUrl = URLEncoder.encode(imageUrl, StandardCharsets.UTF_8.toString()).replace("+", "%20"),
+        previewUrl = URLEncoder.encode(previewUrl, StandardCharsets.UTF_8.toString()).replace("+", "%20"),
+        externalUrl = URLEncoder.encode(externalUrl, StandardCharsets.UTF_8.toString()).replace("+", "%20"),
+        genreNames = genreNames.map {
+            URLEncoder.encode(it, StandardCharsets.UTF_8.toString()).replace("+", "%20")
+        },
+    )
 }
--- feature/create/src/main/java/com/squirtles/feature/create/navigation/CreateNavigation.kt
+++ feature/create/src/main/java/com/squirtles/feature/create/navigation/CreateNavigation.kt
@@ -5,21 +5,14 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import com.squirtles.feature.create.CreatePickScreen
 import com.squirtles.core.model.Song
 import com.squirtles.core.navigation.SearchRoute
 import com.squirtles.core.util.serializableType
-import java.net.URLEncoder
-import java.nio.charset.StandardCharsets
+import com.squirtles.feature.create.CreatePickScreen
 import kotlin.reflect.typeOf
 
 fun NavController.navigateCreate(song: Song, navOptions: NavOptions? = null) {
-    val encodedSong = song.copy(
-        previewUrl = URLEncoder.encode(song.previewUrl, StandardCharsets.UTF_8.toString()),
-        externalUrl = URLEncoder.encode(song.externalUrl, StandardCharsets.UTF_8.toString()),
-        genreNames = song.genreNames.map { URLEncoder.encode(it, StandardCharsets.UTF_8.toString()) },
-        imageUrl = URLEncoder.encode(song.imageUrl, StandardCharsets.UTF_8.toString())
-    )
+    val encodedSong = song.encoded()
     navigate(SearchRoute.Create(encodedSong), navOptions)
 }
 
