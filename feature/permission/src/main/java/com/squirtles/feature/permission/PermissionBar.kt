7fd333fbdbe9e54b3123a4b5a1f5921f7263e6e2
miller198 <miller198@naver.com>
2025-06-26 00:28:50 +0900

    [refactor] import 정리

--- feature/permission/src/main/java/com/squirtles/feature/permission/PermissionBar.kt
+++ feature/permission/src/main/java/com/squirtles/feature/permission/PermissionBar.kt
@@ -14,11 +14,9 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import com.squirtles.core.common.ui.Constants.DEFAULT_PADDING
 
 @Composable
