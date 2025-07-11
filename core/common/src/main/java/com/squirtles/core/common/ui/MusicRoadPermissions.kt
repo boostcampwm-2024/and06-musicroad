ac9a1c5d3464076a7d4c7b024c8d15c8d0bd5fa8
miller198 <miller198@naver.com>
2025-07-11 18:23:51 +0900

    [refactor] 권한 체크 함수 추가
    위치권한 허용 여부 체크 함수

--- core/common/src/main/java/com/squirtles/core/common/ui/MusicRoadPermissions.kt
+++ core/common/src/main/java/com/squirtles/core/common/ui/MusicRoadPermissions.kt
@@ -1,17 +1,26 @@
 package com.squirtles.core.common.ui
 
 import android.Manifest
+import android.content.Context
+import androidx.core.content.PermissionChecker
 
 object MusicRoadPermissions {
     // 선택적 권한
-    val OPTIONAL_PERMISSIONS = listOf<String>()
+    val OPTIONAL_PERMISSIONS = listOf<String>(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+    )
 
     // 필수 권한
     val CORE_PERMISSIONS = listOf<String>(
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION,
         Manifest.permission.RECORD_AUDIO,
     )
 
     val ALL_PERMISSIONS = OPTIONAL_PERMISSIONS + CORE_PERMISSIONS
+
+    fun checkLocationPermission(context: Context): Boolean {
+        return OPTIONAL_PERMISSIONS.all {
+            PermissionChecker.checkSelfPermission(context, it) == PermissionChecker.PERMISSION_GRANTED
+        }
+    }
 }
